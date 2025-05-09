002ab0783b6af72c8ff8a21704dd878b7b9ea42d
mark1yankits <marcusik6509@gmail.com>
2025-05-09 17:45:49 +0300

    add logic for header (Login | Logout)

--- server/src/task/task.controller.ts
+++ server/src/task/task.controller.ts
@@ -51,6 +51,5 @@ export class TaskController {
   remove(@Param('id') id: string) {
     return this.taskService.remove(+id);
   }
-
   
 }
--- server/src/task/task.service.ts
+++ server/src/task/task.service.ts
@@ -99,6 +99,8 @@ export class TaskService {
     })
     return task
   }
+
+
 }
 
 
